97719302d219f1e5243e95217ef0d17d90f1e8e6
dillonl <dillonl49@163.com>
2022-10-24 21:42:53 +0800

    fix[client]:修改前台启动端口号

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    host: "0.0.0.0",
+    port: 80
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        }
+      }
+    }
+  }
 })
