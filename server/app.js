558912065facfc393a22bddd26f0fe495b6d7660
dillonl <dillonl49@163.com>
2022-10-24 23:15:51 +0800

    style:# 增加样式文件

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // 开放 public 目录
 
 
 app.use('/', (req, res) => {
